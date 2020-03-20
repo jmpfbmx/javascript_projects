ae6a7238fea09fd5e64f43a78e43e01ef54fe24d
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-03-20 11:22:28 +0100

    Create BandNamesGenerator.js

---
+++ LOOPS/BandNamesGenerator.js
@@ -0,0 +1,13 @@
+for (var adj of [
+  'Raging',
+  'Happy',
+  'Ugly',
+]) {
+  for (var noun of [
+    'Pharmacists',
+    'Squids',
+    'Twins',
+  ]) {
+    print('The ' + adj + ' ' + noun);
+   }
+}
