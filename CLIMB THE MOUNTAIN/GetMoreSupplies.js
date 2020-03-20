ea73e68d15cd4ebc744ae258d648cfd3d63f725f
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-03-20 12:56:43 +0100

    Create GetMoreSupplies.js

---
+++ CLIMB THE MOUNTAIN/GetMoreSupplies.js
@@ -0,0 +1,19 @@
+var myBackpack = {
+  food: [
+    'bananas',
+    'nuts',
+    energy bar',
+  ],
+  equipment: [
+    'map',
+    'compass',
+  ],
+  clothing: [
+    'scarf',
+    'jacket',
+    'hat',
+  ],
+};
+print(myBackpack.food);
+print(myBackpack.equipment);
+print(myBackpack.clothing);
